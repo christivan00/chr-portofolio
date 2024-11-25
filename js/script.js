bfad4232834e1cd5ec1ff9813effee1f98cee339
christivan00 <157097188+christivan00@users.noreply.github.com>
2024-11-25 12:09:37 +0700

    save 3

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
-
 window.addEventListener("load", () => {
     const body = document.querySelector("body");
     body.classList.add("loaded");
+  });
+  
+  
+const card = document.querySelector(".card");
+
+card.addEventListener("click", () => {
+    card.classList.toggle("flipped");
   });
   
