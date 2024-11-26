d5f95332e5a7534d2d49106a55d18dcd55796c86
christivan00 <157097188+christivan00@users.noreply.github.com>
2024-11-26 22:30:37 +0700

    done

--- js/script.js
+++ js/script.js
@@ -9,4 +9,14 @@ const card = document.querySelector(".card");
 card.addEventListener("click", () => {
     card.classList.toggle("flipped");
   });
-  
+
+const links = document.querySelectorAll(".halus");
+links.forEach(link => {
+      link.addEventListener('click', function(e) {
+          e.preventDefault();
+          const targetid = this.getAttribute('href');
+          document.querySelector(targetid).scrollIntoView({
+              behavior: 'smooth'
+          });
+      });
+  });
